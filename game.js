3642015834299cf487c502636a38205315c2860b
Kim Nguyen <zktnguyen@ucdavis.edu>
2016-11-27 19:53:12 -0800

    first commit

---
+++ game.js
@@ -0,0 +1,28 @@
+$(document).ready(function(){
+
+});
+
+(function($){
+  var Board = function(){
+    // draw the board given the row and col dimensions
+    // plant the mines
+    // determines if the game is over
+
+  };
+
+  var Game = function(){
+    // This deals with everything on the view. Set default values of dimensions, num_bombs
+    // Starts a new Game
+    // initializes the board
+    // triggers the Cheat method by revealing all of the bombs.
+
+
+  };
+
+  var spot = function(x, y, bomb){
+    this.x = x;
+    this.y = y;
+    this.isMine = bomb;
+  }; //
+
+}(jQuery));
