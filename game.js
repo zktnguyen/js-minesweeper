d24fdf9b4d3f395f674a8f1597b23f5c999b28a4
Kim Nguyen <zktnguyen@ucdavis.edu>
2016-11-29 01:34:29 -0800

    board is drawn, binded left and right click, need game logic

--- game.js
+++ game.js
@@ -5,92 +5,180 @@ $(document).ready(function(){
     this.isMine = false;
     this.isRevealed = false;
     this.isFlagged = false;
-    this._id = y+'-'+x;
-    this.mark = '';
+    this.el_id = y+'-'+x;
+    this.text = '';
     this.adj = [];
-    this.adjBombs = 0;
+    this.adjMines = 0;
   };
 
   Field.prototype.setMine = function(bool){
-    this.isMine = true;
+    this.isMine = bool;
+    return this;
   };
 
-  Field.prototype.setVal = function(){
-
+  Field.prototype.setFlagged = function(bool){
+    this.isFlagged = bool;
+    return this;
   };
 
-  var Board = function(dimensions, numBombs){
+  Field.prototype.setRevealed = function(bool){
+    this.isRevealed = bool;
+    if (this.isMine) this.isRevealed = true;
+    return this;
+  };
+
+  // sets the mark of the field as a mine or flagged
+  Field.prototype.setText = function(val){
+    var td = $('.minefield').find('<td id="' + this.el_id + '">');
+    td.removeClass("flag");
+    switch(val){
+      case "!":
+        break;
+      case "?":
+        td.addClass("flag");
+        break;
+      case "X":
+        td.addClass("mine");
+        break;
+    }
+    td.html(val);
+    return this;
+  };
+
+  Field.prototype.flagField = function(){
+    if (this.isRevealed) return;
+    switch(this.text){
+      case "!":
+        this.text = "";
+        break;
+      case "?":
+        this.text = "!";
+        break;
+      case "":
+        this.text = "?";
+        break;
+      }
+    this.setText(this.text || "&nbsp;");
+    return this;
+  };
+
+  Field.prototype.setAdj = function(field){
+    this.adj.push(field);
+    if(field.isMine) this.adjMines++;
+    return this;
+  };
+
+  Field.prototype.getAdj = function(){
+    return this.adj;
+  };
+
+  Field.prototype.reveal = function(){
+    return this.adjMines || this.isMine;
+  };
+
+  var Board = function(dimensions, numMines){
     // draw the board given the row and col dimensions
     // plant the mines
     // determines if the game is over
-    var board = [];
-    var bombs = {};
+    var row;
+    this.board = [];
     this.dimensions = dimensions;
-    this.numBombs = numBombs;
-  };
-    Board.prototype.initBoard = function(){
-      var row;
-
-
-      this.board = [];
-
-      for (var y = 0; y < this.rows; y++){
-        row = [];
-        this.board.row.push(row);
-        for (var x = 0; x < this.cols; x++){
-          row.push(new Field(x,y));
-        }
+    for (var y = 0; y < dimensions; y++){
+      row = [];
+      this.board.push(row);
+      for (var x = 0; x < dimensions; x++){
+        row.push(new Field(x,y));
       }
-      // draw the board
-      // assign neighbors
-    };
+    }
+    this.plantMines(numMines, dimensions);
+  };
 
-    Board.prototype.drawBoard = function(){
+    Board.prototype.drawBoard = function(dimensions){
       var tbody = $("<tbody>");
       var tr, td;
       $(".minefield").empty().html(tbody);
-
-      for (var y = 0; y < this.dimensions; y++){
+      for (var y = 0; y < dimensions; y++){
         tr = $('<tr>');
-        for (var x = 0; x < this.dimensions; x++){
-          td = $('<td data-index="' +y+'-'+x+ '">').html('&nbsp;');
+        for (var x = 0; x < dimensions; x++){
+          td = $('<td id="' +y+'-'+x+ '">').html('&nbsp;');
           tr.append(td);
         }
         tbody.append(tr);
       }
-      console.log("Creation");
       return this;
     };
+
     function getRandom(dimensions){
       return Math.floor(Math.random() * 1000 + 1) % dimensions;
     }
 
-    Board.prototype.plantBombs = function(numBombs){
-      for (var i = 0; i < numBombs; i++){
-          console.log("planting bombs");
+    Board.prototype.plantMines = function(numMines, dimensions){
+      var mines = 0;
+      while(mines < numMines){
+        var x = getRandom(dimensions);
+        var y = getRandom(dimensions);
+        if (!this.board[x][y].isMine){
+          this.board[x][y].setMine(true);
+          mines++;
+        }
       }
     };
-  var Game = {
+
+    Board.prototype.setAdjacent = function(field){
+      var result = [], x = field.x, y=field.y;
+      for (var i = x-1; i < x+1; i++){
+        if (i < 0 || i >= this.dimensions) continue;
+        for (var j = y-1; j < y+1; x++){
+          if (j < 0 || j >= this.dimensions) continue;
+          if (y === j && x === i) continue;
+          result.push(this.getCell(i, j));
+        }
+      }
+      return result;
+    };
+
+    Board.prototype.getField = function(x,y){
+      return this.board[y][x];
+    };
+
+  var Minesweeper = {
     // deals with everything on the view.
-    //Set default values of dimensions, num_bombs
+    //Set default values of dimensions, num_mines
     // Starts a new Game
     // initializes the board
-    // triggers the Cheat method by revealing all of the bombs.
+    // interacts with the buttons (New Game, Reveal Mines)
+    done: false,
+    timeElapsed: 0,
+    complete: function(){
+      this.done = true;
+
+    },
     setDefaults: function() {
       $('#dimensions').val(9);
-      $('#bombs').val(10);
+      $('#mines').val(10);
     },
 
     start: function() {
-      this.dimensions = $('#dimensions').val();
-      this.numBombs = $('#bombs').val();
-      this.board = new Board(this.dimensions, this.numBombs);
-      this.board.drawBoard();
+      var dimensions = $('#dimensions').val();
+      var numMines = $('#mines').val();
+      var board = new Board(dimensions,numMines);
+      board.drawBoard(dimensions);
     }
-
   };
-  Game.setDefaults();
-  Game.start();
+
+
+  Minesweeper.setDefaults();
+  Minesweeper.start();
+  $(".minefield").click(function(e){
+    console.log($(e.target));
+    return false;
+  });
+
+  $(".minefield").bind('contextmenu rightclick', function(e){
+    e.preventDefault();
+    console.log($(e.target));
+    return false;
+  });
 });
 
 // (function($){
